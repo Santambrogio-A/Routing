7bbf07cbe82ad3e058a3778c9b2bc7bead06e02c
Santambrogio-A <santambrogio.andrea@iisgalvanimi.edu.it>
2022-09-14 07:45:57 +0000

    Parte 2 domanda 1

--- Routing-Santambrogio/src/app/fruits/fruits.component.ts
+++ Routing-Santambrogio/src/app/fruits/fruits.component.ts
@@ -5,11 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './fruits.component.html',
   styleUrls: ['./fruits.component.css']
 })
-export class FruitsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class FruitsComponent {
+  fruits = [
+    { name: 'l3ineb', emoji: '🍇' },
+    { name: 'btikha', emoji: '🍈' },
+    { name: 'della7a', emoji: '🍉' },
+    { name: 'limouna', emoji: '🍊' },
+    { name: '7ameDa', emoji: '🍋' },
+    { name: 'banana', emoji: '🍌' },
+    { name: 'ananaSa', emoji: '🍍' },
+    { name: 'teffa7a 7emra', emoji: '🍎' },
+    { name: 'teffa7a kheDra', emoji: '🍏' },
+    { name: 'nGaSa', emoji: '🍐' },
+    { name: 'khokha', emoji: '🍑' },
+    { name: '7eb lmlouk', emoji: '🍒' },
+  ]
 
 }
